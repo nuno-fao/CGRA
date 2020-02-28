60a9010e2a58fd53d4541b4fd5a4d8b7a109eedb
Nuno Oliveira <oliveiranunopt@gmail.com>
2020-02-28 12:06:21 +0000

    started remote rep

---
+++ ex1/MyInterface.js
@@ -0,0 +1,32 @@
+/**
+* MyInterface
+* @constructor
+*/
+class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        // http://workshop.chromeexperiments.com/examples/gui
+        this.gui = new dat.GUI();
+        
+        var obj = this;
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+		this.gui.add(this.scene, 'displayDiamond').name('Diamond');
+		this.gui.add(this.scene, 'displayTriangle').name('Triangle');
+		this.gui.add(this.scene, 'displayTriangleSmall').name('Small Triangle');
+		this.gui.add(this.scene, 'displayTriangleBig').name('Big Triangle');
+		this.gui.add(this.scene, 'displayParallelogram').name('Parallelogram');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        return true;
+    }
+}
